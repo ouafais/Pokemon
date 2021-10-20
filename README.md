d1b55dd7d9b06073f6ddbeffc6ff446c440d40f0
ouafais <56456624+ouafaa1982@users.noreply.github.com>
2021-10-20 21:37:47 +0100

    API images problem
    API from where I get images isnt orking commented the img tag

--- README.md
+++ README.md
@@ -1,7 +1,9 @@
 # Getting Started with Create React App
-App: Pokemons cards
-Api: for the pokemon characteristics used the https://pokeapi.co/api/v2/pokemon API
-     for the images used https://pokeres.bastionbot.org/images/pokemon  API
 
-created by: Ouafaa AISSAOUI 
-Inspired by the robots exercise of the zero to mastery course of the complete web developer  .
+App: Pokemons cards
+description: Simple webpage using the Pokemon API to render all 151 pokemon
+Api: for the pokemon characteristics used the https://pokeapi.co/api/v2/pokemon API
+for the images used https://astiopokeres.bnbot.org/images/pokemon API (there is some problem with this API)
+
+created by: Ouafaa AISSAOUI
+Inspired by the robots exercise of the zero to mastery course of the complete web developer .
--- src/Card.js
+++ src/Card.js
@@ -6,7 +6,7 @@ class  Card extends Component {
 
   state ={
     pokemonID : this.props.id,
-    pokemon: {}
+    pokemon: {},
   }
 
   componentDidMount(){
@@ -15,17 +15,23 @@ class  Card extends Component {
       return response.json()
     })
     .then((result)=> {
-      this.setState({pokemon: result})
+        this.setState({pokemon: result})
+        
     })
   }
+  
+  
 
     
-  render () {return (
+  render () {
+    return (
        <div className='pokemonCard'>        
-        <img alt='pokemon' src={`https://pokeres.bastionbot.org/images/pokemon/${this.state.pokemonID}.png?`} width="200" height="200" />
-        <span className='name'>{this.state.pokemon.name} </span>        
+        {/*<img alt='pokemon' src={`https://pokeres.bastionbot.org/images/pokemon/${this.state.pokemonID}.png?`} width="200" height="200" />*/}
+        <span className='name'>{this.state.pokemon.name} </span>  
+        
        </div>
-     )}
+     )
+  }
 
 };
 
